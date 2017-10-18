b7d48390515383991162f9675a538bee6b2bc876
miekroel <mieke.roelandt@student.arteveldehs.be>
2017-10-18 23:38:37 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<style>
+.grijs{
+    width :50px;
+    height:50px;
+    float: left;
+    
+  
+}
+</style>
+    <div id="div">
+    <?php
+    for($i = 0;  $i < 50; ++$i ){
+        $color = $i*5;
+       // $color = "rgba(${colorMath},${colorMath},${colorMath},1)";
+        echo `<div class="grijs" style="background-color: 'rgba(${color}, ${color}, ${color},1)'"></div>`;
+    }
+
+
+?>
+    
+    
+    </div>
+</body>
+</html>
